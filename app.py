b1a4caa198fb93537860a6e7642b66da5d3d76cd
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2025-08-15 17:42:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
 # app.py
 
 import streamlit as st
-from streamlit.web.server.server import Server
 
-# Esta é a configuração da página que será mostrada ANTES do menu ser selecionado
-st.set_page_config(
-    page_title="Início",
-    page_icon="📚",
-    layout="wide"
-)
-
-st.title("Bem-vindo ao Painel Educacional do ES")
-st.sidebar.success("Selecione uma das páginas acima.")
-
-st.markdown(
-    """
-    Este projeto apresenta dados sobre as escolas do Espírito Santo.
-    **👈 Selecione uma das opções no menu lateral** para começar a explorar.
-    ### Quer saber mais?
-    - Confira a página de **Apresentação** para detalhes do projeto.
-    - Explore o **Mapa Interativo** para localizar as escolas.
-    """
-)
+# Este comando define qual página será carregada por padrão
+# O caminho é relativo à pasta raiz do projeto
+st.switch_page("pages/0_🏠_Apresentação.py")
