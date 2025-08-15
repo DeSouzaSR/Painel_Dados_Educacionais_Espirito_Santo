eb809c05ede45210d096f21c3ea5b1c7bd41e7bf
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2025-08-15 17:15:53 -0300

    Create 2_Estatisticas_da_Escola.py

---
+++ pages/2_Estatisticas_da_Escola.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.title("📊 Estatísticas das Escolas")
+
+st.info("Esta página está em construção. Em breve, você poderá selecionar uma escola e visualizar suas principais estatísticas, como etapas de ensino, capacidade de alunos, etc.")
