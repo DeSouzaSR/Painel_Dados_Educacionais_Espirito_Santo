6ece21d7cd19c073042a9a44bfe6dbd32d681fc9
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2025-08-15 17:16:41 -0300

    Create 3_Indicadores_de_Qualidade.py

---
+++ pages/3_Indicadores_de_Qualidade.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.title("Indicadores de Qualidade (IDEB)")
+
+st.info("Esta página está em construção. Em breve, você poderá consultar os indicadores educacionais, como a nota do IDEB, para as escolas do estado.")
