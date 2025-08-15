2fc599852cb339d99cd91c5b02f48da831d28177
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2025-08-15 17:43:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import streamlit as st
 
 # Este comando define qual página será carregada por padrão
 # O caminho é relativo à pasta raiz do projeto
-st.switch_page("pages/0_🏠_Apresentação.py")
+st.switch_page("pages/0_Apresentação.py")
