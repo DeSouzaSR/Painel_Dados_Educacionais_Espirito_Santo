b5d60e6f6880e84e91840a806b23f1cc3b06511a
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2025-08-15 17:15:16 -0300

    Create 1_Mapa_Interativo.py

---
+++ pages/1_Mapa_Interativo.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.title("📍 Mapa Interativo das Escolas")
+
+st.info("Esta página está em construção. Em breve, você verá um mapa interativo com a geolocalização de todas as escolas do Espírito Santo.")
